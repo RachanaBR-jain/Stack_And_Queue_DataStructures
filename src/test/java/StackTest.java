924e97c44573b0133e68a85f7f120b681edc00d8
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-13 02:06:11 +0530

    Added code to pop elements from stack and check wheather stack is emplty or not

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -3,18 +3,31 @@ import org.junit.jupiter.api.Test;
 
 public class StackTest {
 
+    static  Stack stack = new Stack();
 
     @Test
-    public void top_element_mustBePrintedFirst_When_pushed_3Values() {
+    public static void top_element_mustBePrintedFirst_When_pushed_3Values() {
         Node<Integer> myFirstNode = new Node<>(70);
         Node<Integer> mySecondNode = new Node<>(30);
         Node<Integer> myThirdNode = new Node<>(56);
 
-        Stack stack = new Stack();
         stack.push(myFirstNode);
         stack.push(mySecondNode);
         stack.push(myThirdNode);
         stack.list();
     }
+
+
+    @Test
+    public void given3NumberInStack_WhenPoped_ShouldReturnEmpty() {
+        top_element_mustBePrintedFirst_When_pushed_3Values();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        INode peak = stack.peak();
+        System.out.println(" After pop "+ peak );
+        Assertions.assertEquals(null, peak);
+    }
+
 
 }
