2dae23fa2b3241dc0f8d2b0b180d1b6c93ace0fb
RachanaBR-jain <br19rachanajain@gmail.com>
2021-03-19 08:36:32 +0530

    added code to dequeue elements from queue

--- src/test/java/QueueTest.java
+++ src/test/java/QueueTest.java
@@ -21,4 +21,32 @@ public class QueueTest {
         Assertions.assertEquals(myFirstNode, peak);
     }
 
+
+    @Test
+    public void given3Number_WhenAddedToQueue_ShouldDequeueTheNodes() {
+        Node<Integer> myFirstNode = new Node<>(56);
+        Node<Integer> mySecondNode = new Node<>(30);
+        Node<Integer> myThirdNode = new Node<>(70);
+
+        Queue myQueue = new Queue();
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+
+        myQueue.dequeue();
+        System.out.println("1st dequeue");
+        myQueue.printQueue();
+        System.out.println("2nd dequeue");
+        myQueue.dequeue();
+        myQueue.printQueue();
+        System.out.println("3rd dequeue");
+        myQueue.dequeue();
+        System.out.println("empty queue");
+        INode peak;
+        peak = myQueue.peak();
+        Assertions.assertNull(peak);
+
+    }
+
 }
